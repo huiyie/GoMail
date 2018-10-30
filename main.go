680d4285defdf457058aacb4383206e81482c236
huiyie <huiyiechng14@gmail.com>
2018-10-30 00:27:43 -0700

    Use library router

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 import (
-  "net/http"
+	"log"
+	"net/http"
+	
+	"github.com/gorilla/mux"
 )
 func sayHello(w http.ResponseWriter, r *http.Request) {
-  message := "Hello world"
+	message := "Hello world"
+	w.WriteHeader(200)
   w.Write([]byte(message))
 }
 func main() {
-  http.HandleFunc("/", sayHello)
-  if err := http.ListenAndServe(":8080", nil); err != nil {
-    panic(err)
-  }
+	router := mux.NewRouter()
+	router.HandleFunc("/", sayHello).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", router))
+  // http.HandleFunc("/", sayHello)
+  // if err := http.ListenAndServe(":8080", nil); err != nil {
+  //   panic(err)
+  // }
 }
