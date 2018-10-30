9b7ac4b7bb02a64ec784a5a54445d7f123e57dc7
huiyie <huiyiechng14@gmail.com>
2018-10-30 00:29:35 -0700

    fixup

--- main.go
+++ main.go
@@ -14,8 +14,4 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", sayHello).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", router))
-  // http.HandleFunc("/", sayHello)
-  // if err := http.ListenAndServe(":8080", nil); err != nil {
-  //   panic(err)
-  // }
 }
